1d886eb2d796d35cc52f396ab8f08e10a2a41e79
Gateway LT <slippy17@hotmail.com>
2026-03-22 01:41:55 +0000

    updates on INSERT from CD (Musicbrainzingzs) to mySQL to handle multi CDs and odd characters in Album title

--- Tools/INSERT_cd_to_MySQL.py
+++ Tools/INSERT_cd_to_MySQL.py
@@ -1,23 +1,35 @@
 import discid
 import musicbrainzngs
 import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, event
 ##############################################################################
 ## Algorithm                                                                
 ## 1) Get CD info from MusicBrainzngs.
 ## 2) Check if artist exists in artists table.
 ## 3) INSERT artist into artists table if not already there.
 ## 4) Get artist ID
-## 5) Check if album exsists in album table against that artist ID.
+## 5) Check if album exists in album table against that artist ID.
 ## 6) INSERT album with artist ID into albums table if not already there.
 ## 7) Get album ID
 ## 8) INSERT tracks with album ID into tracks table
+## 9) Perform steps 5 to 8 for multiple CD albums. 
 
 # -----------------------------
 # Database connection
 # -----------------------------
-engine = create_engine("mysql+pymysql://root:123@192.168.0.115/cddb", echo=False)
+#engine = create_engine("mysql+pymysql://root:123@192.168.0.115/cddb", echo=False)
+engine = create_engine(
+    "mysql+pymysql://root:123@192.168.0.115/cddb?charset=utf8mb4", echo=False
+)
 
+@event.listens_for(engine, "connect")
+def set_utf8mb4(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("SET NAMES utf8mb4")
+    cursor.execute("SET character_set_connection = utf8mb4")
+    cursor.execute("SET character_set_client = utf8mb4")
+    cursor.execute("SET character_set_results = utf8mb4")
+    cursor.close()
 
 
 # -----------------------------
@@ -32,32 +44,40 @@ def fetch_cd_metadata():
     except musicbrainzngs.ResponseError:
         print("Disc not found or bad response")
         return None
-
-    if "disc" in cd_data:
-        release = cd_data["disc"]["release-list"][0]
-        artist_name = release['artist-credit-phrase']
-        album_name = release['title']
-        tracks = release['medium-list'][0]['track-list']
-        print(f"CD Artist:\t{artist_name}")
-        print(f"CD Album title:\t{album_name}\n")
-        track_list=[]
-        for track in tracks:
+    
+    is_multi = len(cd_data['disc']['release-list'][0]['medium-list']) ##num of disks on a multi-album
+    cd_obj=[] 
+    
+    for cd in range(is_multi):
+        if "disc" in cd_data:
+            release = cd_data["disc"]["release-list"][0]
+            artist_name = release['artist-credit-phrase']
+            album_name = release['title']
+            if is_multi>1: album_name = album_name + " "+ str(cd+1) # add a 1 or 2 etc to the Album title.
+            tracks = release['medium-list'][cd]['track-list']
+            print(f"CD Artist:\t{artist_name}")
+            print(f"CD Album title:\t{album_name}\n")
+            track_list=[]
+            for track in tracks:
                 song_title = track['recording']['title']
                 length = int(track['recording'].get('length', 0))
                 position = int(track['position'])
                 #print (artist_name, album_name, position, song_title, length)
                 track_list.append([position, song_title, length])
-        cd_dict= {
-            "album": album_name,
-            "artist": artist_name,
-            "tracks": track_list
-            }
-        
-    elif "cdstub" in cd_data:
-        stub = cd_data["cdstub"]
-        print(f"CD Stub Title:\t{stub.get('title', 'Unknown')}")
+            cd_dict= {
+                "album": album_name,
+                "artist": artist_name,
+                "tracks": track_list,
+                "multi": is_multi
+                }
+            cd_obj.append(cd_dict)
+
+            
+        elif "cdstub" in cd_data:
+            stub = cd_data["cdstub"]
+            print(f"CD Stub Title:\t{stub.get('title', 'Unknown')}")
     
-    return cd_dict
+    return cd_obj
 
 def check_artist(artist_name):
     query = "SELECT * FROM artists WHERE artist="+artist_name
@@ -77,21 +97,41 @@ def insert_artist(artist_name):
         return
     
 def check_album(album_name):
-    query = "SELECT * FROM albums WHERE album_title="+album_name
+    query = text("SELECT * FROM albums WHERE album_title= :title")
     with engine.connect() as conn:
-        result = conn.execute(text(query))
+        result = conn.execute(query, {"title":album_name})
         output= result.all()
         if len(output):
             return output
         else:
             return -1
 
+# def insert_album(artist_id, album_title):
+#     query = "INSERT INTO albums(artist_id,album_title,disc_id ) VALUES ("+artist_id+","+album_title+",16)"
+#     with engine.connect() as conn:
+#         conn.execute(text("SET NAMES utf8mb4"))
+#         conn.execute(text(query))
+#         conn.commit()
+#         return
+    
 def insert_album(artist_id, album_title):
-    query = "INSERT INTO albums(artist_id,album_title ) VALUES ("+artist_id+","+album_title+")"
+    album_title = album_title.replace("‐", "-")  # Replace U+2010 with ASCII hyphen
+    query = text("""
+        INSERT INTO albums (artist_id, album_title, disc_id)
+        VALUES (:artist_id, :album_title, :disc_id)
+    """)
+
     with engine.connect() as conn:
-        conn.execute(text(query))
-        conn.commit()
-        return
+        # no need to do SET NAMES if engine charset is utf8mb4
+        conn.execute(query, {
+            "artist_id": artist_id,
+            "album_title": album_title,
+            "disc_id": 0
+        })
+        conn.commit()    
+    
+    
+
     
 def insert_tracks(album_id, tracks):
     for track in tracks:
@@ -109,52 +149,55 @@ def insert_tracks(album_id, tracks):
 
 ## 1) Get CD info from MusicBrainzngs.
 cd_info = fetch_cd_metadata()
+num_cds = cd_info[0]['multi']
 
-artist_name =  "'"+cd_info['artist']+"'"
-album_name = "'"+cd_info['album']+"'"
-tracks = cd_info['tracks']
+for cd in range (num_cds):
 
-## 2) Check if artist exists in artists table.
-if check_artist(artist_name) == -1:
-    print('Artist does not exist in artists table.')
-    x = input(f'Would you like to add {artist_name} to the artists table? Y/N> ')
-    if not x.lower()=='y':
-        print("*** OK: artists table in database not changed ***")
-        exit()
-## 3) INSERT artist into artists table if not already there.
-    insert_artist(artist_name)
-    print(f"*** OK: artist {artist_name} added to artists table in database. ***")
-else: print("*** Artists exists in the artists table in database not changed ***")
+    artist_name =  "'"+cd_info[cd]['artist']+"'"
+    album_name = cd_info[cd]['album']
+    album_name = album_name.replace("‐", "-")  # Replace U+2010 with ASCII hyphen
+    tracks = cd_info[cd]['tracks']
+    ## 2) Check if artist exists in artists table.
+    if check_artist(artist_name) == -1:
+        print('Artist does not exist in artists table.')
+        x = input(f'Would you like to add {artist_name} to the artists table? Y/N> ')
+        if not x.lower()=='y':
+            print("*** OK: artists table in database not changed ***")
+            break
+    ## 3) INSERT artist into artists table if not already there.
+        insert_artist(artist_name)
+        print(f"*** OK: artist {artist_name} added to artists table in database. ***")
+    else: print("*** Artists exists in the artists table in database not changed ***")
 
-## 4) Get artist ID
-art_info = check_artist(artist_name)
-artist_id = str(art_info[0][0])
-##artist_name = art_info[0][1]
+    ## 4) Get artist ID
+    art_info = check_artist(artist_name)
+    artist_id = str(art_info[0][0])
+    ##artist_name = art_info[0][1]
     
-## 5) Check if album exsists in album table 
-album_info = check_album(album_name)
-if album_info == -1:
-    print('Album does not exist in album table.')
-    x = input(f'Would you like to add {album_name} to the albums table under Artist {artist_name}? Y/N> ')
-    if not x.lower()=='y':
-        print("*** OK albums table in database not changed ***")
-        exit()
-## 6) INSERT album with artist ID into albums table if not already there.
-    insert_album(artist_id, album_name)
-    print(f"*** OK: album {album_name} added to albums table in database. ***")
+    ## 5) Check if album exsists in album table 
     album_info = check_album(album_name)
-else: print("*** Album exists in the album table in database not changed ***")
+    if album_info == -1:
+        print('Album does not exist in album table.')
+        x = input(f'Would you like to add {album_name} to the albums table under Artist {artist_name}? Y/N> ')
+        if not x.lower()=='y':
+            print("*** OK albums table in database not changed ***\n")
+            continue
+## 6) INSERT album with artist ID into albums table if not already there.
+        insert_album(artist_id, album_name)
+        print(f"*** OK: album {album_name} added to albums table in database. ***")
+        album_info = check_album(album_name)
+    else: print("*** Album exists in the album table in database not changed ***")
 
 ## 7) Get album ID
-album_id = str(album_info[0][0])
+    album_id = str(album_info[0][0])
 
 
 ## 8) INSERT tracks with album ID into tracks table
 
-x = input(f'Would you like to add tracks to the tracks table under Artist {artist_name}? Y/N> ')
-if not x.lower()=='y':
-    print("*** OK tracks table in database not changed ***")
-    exit()
-insert_tracks(album_id, tracks)
+    x = input(f'Would you like to add tracks to the tracks table under Artist {artist_name}? Y/N> ')
+    if not x.lower()=='y':
+        print("*** OK tracks table in database not changed ***")
+        break
+    insert_tracks(album_id, tracks)
     
     
---
+++ Tools/pkl_from_MySQL_DB.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+##############################################################################
+## Algorithm                                                                
+## 1) Load pickle file
+## 2) Use columns from pickle file to buid SELECT query.
+## 3) Connect to DB
+## 4) Run SELECT query + Capture the query in a dataframe
+## 5) Convert the dataframe to match format of pkl file.
+## 6) Save as pickle file.
+##############################################################################
+
+## 1) Load pickle file
+df = pd.read_pickle("../static/cd_database.pkl")
+
+## 2) Use columns from pickle file.
+cols = df.columns
+
+# -----------------------------
+# Database connection
+## 3) Connect to DB
+# -----------------------------
+engine = create_engine("mysql+pymysql://root:123@192.168.0.115/cddb", echo=False)
+
+
+def query_db():
+    query = "select * from v_song_catalog;"
+    with engine.connect() as conn:
+        result = conn.execute(text(query))
+        output= result.all()
+        
+        return output
+    
+def convert_cols(cols,db):
+    db = db.drop("track_id", axis=1)            ## drop track_id
+    db = db.reindex(sorted(db.columns), axis=1) ## sort db columns
+    c = cols.sort_values()                      ## sort df columns from pkl so they match
+    db = db.set_axis(c, axis=1)                 ## rename db columns as in pkl file
+    db = db[cols]                               ## put columns back in order as orginal pkl
+    db['Disc_ID'] = db['Disc_ID'].fillna(0)     ## Disc_ID became a float. Need to make Nulls a 0
+    #db = db.drop(db[db["Disc_ID"] == 0].index)  ## Drop Disc_ID=0 as CD not in CD player.
+    db['Disc_ID'] = db['Disc_ID'].astype('int64')# Convert to int64
+    db['Track_ID'] = db['Track_ID'].astype('object')
+    db['Length'] = db['Length'].astype('object')
+    
+    return db
+
+def show_cd_slots(df):
+    
+    unique_discs = df[df['Disc_ID'].between(1, 101)].drop_duplicates(subset=['Disc_ID'])
+    display_df = unique_discs.drop(columns=['Length', 'Artist','Song_Title', 'Track_ID'])
+    display_df = display_df.sort_values(by='Disc_ID')
+    print(display_df.to_string(index=False))
+    return display_df
+
+def compare_dfs(df,df2):
+    df1 = df.copy()
+    cond = df1['Album'].isin(df2['Album'])
+    df1.drop(df1[cond].index, inplace = True)
+    return df1
+
+## 4) Run SELECT query + Capture the query in a dataframe
+db = pd.DataFrame(query_db())
+
+## 5) Convert the dataframe to match format of pkl file.
+db = convert_cols(cols,db)
+
+## 6) Save as pickle file.
+#pd.to_pickle(db, "../static/cddb.pkl")
+
+
+## show albums in slot 0
+##   db[db["Disc_ID"] == 0].drop_duplicates(subset=['Album']
+
